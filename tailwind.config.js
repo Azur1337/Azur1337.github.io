b17ee9c5e465836ef3b4b7c883de98111d09cea4
Azur1337 <82388136+Azur1337@users.noreply.github.com>
2023-07-31 18:06:36 +0200

    Publish

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: 'class',
+  content: ["./src/**/*.html"],
+  theme: {
+    container: {
+    },
+    colors: {
+      femboypink:'#cdb4db',
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
+
